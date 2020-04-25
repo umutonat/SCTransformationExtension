32a04f20c2f8cb36b809b61dbef1a9e70fba87b9
Umut Onat Artuvan <umutonat@hotmail.de>
2020-04-25 12:02:14 +0200

    Initial commit

---
+++ src/extension.ts
@@ -0,0 +1,34 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	// This line of code will only be executed once when your extension is activated
+	console.log('Congratulations, your extension "SCTransformationExtension" is now active!');
+
+	var setting: vscode.Uri = vscode.Uri.parse("/Users/artuvan/Github/SmartContractDescriptorsGenerator/SCTransformation/Resources/in.sol");
+vscode.workspace.openTextDocument(setting).then((a: vscode.TextDocument) => {
+    vscode.window.showTextDocument(a, 2, false);
+}, (error: any) => {
+    console.error(error);
+    debugger;
+});
+	// The command has been defined in the package.json file
+	// Now provide the implementation of the command with registerCommand
+	// The commandId parameter must match the command field in package.json
+	let disposable = vscode.commands.registerCommand('SCTransformationExtension.helloWorld', () => {
+		// The code you place here will be executed every time your command is executed
+
+		// Display a message box to the user
+		vscode.window.showInformationMessage('Hello World from SCTransformation!');
+	});
+
+	context.subscriptions.push(disposable);
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {}
